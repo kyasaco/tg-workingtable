56dd63bc6f570e7cf6bdd0dace9a9fbeebbdb851
tg_yuuma_kasuda <yuuma_kasuda@technical-g.co.jp>
2020-11-13 14:10:29 +0900

    メイン画面のリロード時

--- src/main/resources/static/js/main_js.js
+++ src/main/resources/static/js/main_js.js
@@ -7,3 +7,13 @@ var  right_time = parseFloat(times[1].textContent)
 if(left_time > right_time){
 	times[0].style.color = 'red';
 }
+
+$(window).scroll(function() {
+  sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function() {
+  if (sessionStorage.scrollTop != "undefined") {
+    $(window).scrollTop(sessionStorage.scrollTop);
+  }
+});
